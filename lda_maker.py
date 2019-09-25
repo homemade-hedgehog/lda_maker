ff37f4648e4f3f0465b78326101c189d9270a981
homemade-hedgehog <homemade.hedgehog@gmail.com>
2019-09-26 00:13:50 +0900

    new_func remove_stops, fix_func remove_stops
    stop word 除去関数作成

---
+++ lda_maker.py
@@ -0,0 +1,28 @@
+import sentencepiece as spm
+from gensim.models import LdaMulticore
+from gensim.corpora.dictionary import Dictionary
+from tqdm import tqdm
+import numpy as np
+from collections import Counter, defaultdict
+import gc
+import os
+import logging
+import hashlib
+import time
+import joblib
+import re
+
+
+def remove_stops(tokens: list, dict_is_stops: dict) -> list:
+    """
+    stop wordをlist of token から除去する
+    stop word(有ってもなくても文意が変わらない or あらゆる文書に現れるため特徴にならない)
+
+    stop wordか否かをいちいちlist探索＆ifで除去していると時間がかかるので、
+    pythonのdictの組み込み関数getで含めばvalue, 含まなければ指定値を返すのを使う
+    :param tokens: list of str, token の　list
+    :param dict_is_stops: dict, {stop_word: True}が入ったdict, stop wordしか持たない
+    :return: list, stop word 除去済みtokens
+    """
+    list_not_stop = [not dict_is_stops.get(token, False) for token in tokens]
+    return np.array(tokens, dtype=object)[list_not_stop].tolist()

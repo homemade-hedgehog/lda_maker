f6d535a1c3c62294df291fe5396c4685e6ab08e9
homemade-hedgehog <homemade.hedgehog@gmail.com>
2019-09-26 01:02:54 +0900

    new_func search_frequent_token, fix_func search_frequent_token
    tokens_listから頻出語を抽出して、{頻出語: True, ...}の形式で返す。
    この辞書は頻出語をキーに持ち、値はTrue

--- lda_maker.py
+++ lda_maker.py
@@ -41,7 +41,7 @@ def leave_valid(tokens: list, dict_is_valid: dict) -> list:
     return np.array(tokens, dtype=object)[list_valid].tolist()
 
 
-def init_sentence_piece(file_name_sentence_piece_model: str) ->spm.SentencePieceProcessor:
+def init_sentence_piece(file_name_sentence_piece_model: str) -> spm.SentencePieceProcessor:
     """
     sentencepiece model loader
     :param file_name_sentence_piece_model: str, path to sentencepiece model file
@@ -61,3 +61,33 @@ def aggregate_all_token(tokens_list: list) -> list:
     all_token = []
     _ = [all_token.extend(tokens) for tokens in tokens_list]
     return all_token
+
+
+def search_frequent_token(tokens_list: list, threshold_remove_doc_freq_rate_over_this: float) -> dict:
+    """
+    いろいろな文書に頻出する語は、文書を象徴する語にはならない
+    文書出現割合の高いtokenはstop wordに設定
+    てきとー、超テキトーに作った
+
+    :param tokens_list: list of list of str, 1文書をtokenizeしたlistを全文書でlist化
+    :param threshold_remove_doc_freq_rate_over_this: float(0-1), この割合以上の出現率の語をstop word入り
+    :return:dict, {stop_word: True}, この辞書は頻出語をキーに持ち、値はTrue
+    """
+    # TODO いつかまともに考えるかも
+    dict_doc_freq = {}
+    dict_freq_vs_token = defaultdict(list)
+    for tokens in tokens_list:
+        for token in set(tokens):
+            dict_doc_freq[token] = dict_doc_freq.get(token, 0) + 1
+    for token, doc_freq in dict_doc_freq.items():
+        dict_freq_vs_token[doc_freq].append(token)
+    del dict_doc_freq
+
+    # 上位の方は大体カウント値に対して1語が当たっていて、頻出top n rateを弾く
+    num_remove = len(tokens_list) * threshold_remove_doc_freq_rate_over_this
+    stops = []
+    _ = [stops.extend(tokens) for doc_freq, tokens in dict_freq_vs_token.items() if doc_freq > num_remove]
+
+    dict_is_stops = {token: True for token in stops}
+
+    return dict_is_stops
